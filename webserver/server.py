7100f8d1fe159761a721391a057ca78a39aab405
tuck-ab <alext.5@hotmail.co.uk>
2021-02-08 16:25:26 +0000

    Setting up the static folder

--- webserver/server.py
+++ webserver/server.py
@@ -6,7 +6,13 @@ from flask import render_template
 class WebServer:
     def __init__(self, name):
         template_dir = os.path.dirname(__file__) + "/templates"
-        self.app = Flask(name, template_folder=template_dir)
+        static_dir = os.path.dirname(__file__) + "/static"
+        
+        self.app = Flask(name, 
+                         template_folder=template_dir,
+                         static_folder=static_dir,
+                         static_url_path="/webserver/static",
+        )
         
         self.app.add_url_rule("/", "index", WebServer.index) 
 
