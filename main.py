cf340f8a206abf18e706b86e5d6b45882a7775a1
tuck-ab <alext.5@hotmail.co.uk>
2021-02-11 20:57:26 +0000

    Added attendee meeting join

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, emit
 
 import classes
 
-## -- Mapping pages
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "SecretKey"
 
 socketio = SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*")
+
+
+## -- Mapping pages
 
 @app.route("/")
 def index():
@@ -21,17 +24,20 @@ def index():
 def create_meeting():
     return render_template("create.html")
 
-@app.route("/join")
+@app.route("/join", methods=["POST"])
 def join_meeting():
     ## -- redirect to attendee page using code as jintja parameter
-    pass
+    code = request.form["meetingCode"]
 
-@app.route("/meeting")
+    return redirect(url_for("attendee_page", code=code))
+
+@app.route("/meeting/host")
 def host_page():
     return render_template("host.html") 
 
-def attendee_page():
-    return render_template("attendee.html")
+@app.route("/meeting/attendee/<code>")
+def attendee_page(code):
+    return render_template("attendee.html", meeting_code=code)
 
 
 ## -- Web Socket functions
@@ -39,8 +45,11 @@ def attendee_page():
 @socketio.on("connect")
 def connected():
     print("recieved")
-    
+
     emit("connection_response", {"msg":"response recorded"})
 
+
+
+## -- Running the server
 if __name__ == "__main__":
     socketio.run(app)
