829d0aaa2d0ee158bfd56a61cf738424f3871216
KalyanJoseph <77448029+KalyanJoseph@users.noreply.github.com>
2021-02-17 14:46:24 +0000

    Update emojiMoodObj.py

--- classes/feedbackClasses/emojiMoodObj.py
+++ classes/feedbackClasses/emojiMoodObj.py
@@ -13,12 +13,3 @@ class EmojiMood(moodObj.Mood):
         print("we are now setting a new mood score")
         self.moodScore = score
         return self.moodScore
-
-
-test = EmojiMood(True, 100101, 0.7, "this is the emoji")
-
-print(test.getMoodScore())
-print(test.getAnon())
-print(test.getAttendee())
-print(test.getMoodEmoji())
-print(test.setEmojiMoodScore(0.8))
