c6100fd3fa170e62b6bfb3cfb325bdf3d5a52cef
KalyanJoseph <77448029+KalyanJoseph@users.noreply.github.com>
2021-02-17 17:55:13 +0000

    Update emojiPromptObj.py

--- classes/promptClasses/emojiPromptObj.py
+++ classes/promptClasses/emojiPromptObj.py
@@ -14,13 +14,3 @@ class EmojiPrompt(generalPromptObj.GeneralPrompt):
     
     def removeEmoji(self,emoji):
         self.allowedEmojis.remove(emoji)
-
-
-testList = [1,3,5,6]
-test = EmojiPrompt(1010101, "this is the text", testList)
-
-print(test.getAllowedEmojis())
-test.addEmoji(2)
-print(test.getAllowedEmojis())
-test.removeEmoji(3)
-print(test.getAllowedEmojis())
