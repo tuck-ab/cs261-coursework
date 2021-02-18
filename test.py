eac6080edfb9cf2c2ad554a30bc08feef2ed3cbc
KalyanJoseph <77448029+KalyanJoseph@users.noreply.github.com>
2021-02-18 15:20:54 +0000

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import classes
+
+test = TextMood(True, 100101, 0.7, "this is the text")
+
+print(test.getMoodScore())
+print(test.getAnon())
+print(test.getAttendee())
+print(test.getMoodText())
+print(test.setTextMoodScore(0.8))
