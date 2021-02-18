d47d31476c30c8548cd917df9bee587a5e468f2b
KalyanJoseph <77448029+KalyanJoseph@users.noreply.github.com>
2021-02-18 15:55:05 +0000

    Update multChoiceResponseObj.py

--- classes/feedbackClasses/multChoiceResponseObj.py
+++ classes/feedbackClasses/multChoiceResponseObj.py
@@ -1,6 +1,6 @@
 from .responseObj import Response
 
-class MultChoiceResponse(responseObj.Response):
+class MultChoiceResponse(Response):
 
     def __init__(self, anonFlag, attendeeID, responsePrompt, responseChoice):
         super().__init__(anonFlag, attendeeID, responsePrompt)
