2fd097f5f55c0cdfd7f001f99e5e58d84e22e9ea
Robbie Marseglia <u1912236@live.warwick.ac.uk>
2021-02-24 11:22:39 +0000

    Update test to work for questions

--- dbtest.py
+++ dbtest.py
@@ -2,8 +2,8 @@ from classes import *
 
 connection = DBController()
 
-# testError = ErrorFeedback(True,1,1,"TEST","Just testing the feedback!")
-# connection.insertError(testError)
+testError = ErrorFeedback(True,1,1,"Test","Just testing the feedback!")
+connection.insertError(testError)
 
 testQuestion = QuestionFeedback(False,2,3,"What is a rational agent?")
 connection.insertQuestion(testQuestion)
