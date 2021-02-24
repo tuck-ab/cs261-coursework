923ec3a4e259c0a9e39d8e6afe4ccca1016d7afb
KalyanJoseph <kalyanjoseph@outlook.com>
2021-02-24 12:10:23 +0000

    updated tests and classes with mood and response type

--- classes/feedbackClasses/moodObj.py
+++ classes/feedbackClasses/moodObj.py
@@ -2,9 +2,13 @@ from .generalFeedbackObj import GeneralFeedback
 
 class Mood(GeneralFeedback):
 
-    def __init__(self, anonFlag, attendeeID, meetingID, moodScore):
+    def __init__(self, anonFlag, attendeeID, meetingID, moodType, moodScore):
         super().__init__(anonFlag, attendeeID, meetingID)
+        self.moodType = moodType
         self.moodScore = moodScore
     
+    def getMoodType(self):
+        return self.moodType
+        
     def getMoodScore(self):
         return self.moodScore
--- classes/feedbackClasses/responseObj.py
+++ classes/feedbackClasses/responseObj.py
@@ -2,9 +2,13 @@ from .generalFeedbackObj import GeneralFeedback
 
 class Response(GeneralFeedback):
 
-    def __init__(self, anonFlag, attendeeID, meetingID, responsePrompt):
+    def __init__(self, anonFlag, attendeeID, meetingID, responseType, responsePrompt):
         super().__init__(anonFlag, attendeeID, meetingID)
+        self.responseType = responseType
         self.responsePrompt = responsePrompt
     
+    def getResponseType(self):
+        return self.responseType
+        
     def getResponsePrompt(self):
         return self.responsePrompt
