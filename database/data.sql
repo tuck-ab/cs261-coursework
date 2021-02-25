32a665c1bd050e47dfd28df489abc3c3d6cf97e8
Robbie Marseglia <u1912236@live.warwick.ac.uk>
2021-02-25 12:09:25 +0000

    Created methods to create event and add an attendee to an event

--- database/data.sql
+++ database/data.sql
@@ -24,37 +24,37 @@ INSERT INTO attendees VALUES(2,4);
 INSERT INTO attendees VALUES(3,4);
 INSERT INTO attendees VALUES(4,4);
 
-INSERT INTO feedback VALUES(1,1,1,"Error",1);
-INSERT INTO feedback VALUES(2,1,2,"Question",1);
-INSERT INTO feedback VALUES(3,1,3,"Mood",0);
-INSERT INTO feedback VALUES(4,1,4,"Response",0);
-INSERT INTO feedback VALUES(5,2,1,"Error",0);
-INSERT INTO feedback VALUES(6,2,2,"Question",0);
-INSERT INTO feedback VALUES(7,2,3,"Mood",1);
-INSERT INTO feedback VALUES(8,2,4,"Response",1);
-INSERT INTO feedback VALUES(9,3,1,"Error",1);
-INSERT INTO feedback VALUES(10,3,2,"Question",0);
-INSERT INTO feedback VALUES(11,3,3,"Mood",1);
-INSERT INTO feedback VALUES(12,3,4,"Response",0);
-INSERT INTO feedback VALUES(13,4,1,"Error",0);
-INSERT INTO feedback VALUES(14,4,2,"Question",1);
-INSERT INTO feedback VALUES(15,4,3,"Mood",0);
-INSERT INTO feedback VALUES(16,4,4,"Response",1);
+INSERT INTO feedback VALUES(1,1,1,"error",1);
+INSERT INTO feedback VALUES(2,1,2,"question",1);
+INSERT INTO feedback VALUES(3,1,3,"mood",0);
+INSERT INTO feedback VALUES(4,1,4,"response",0);
+INSERT INTO feedback VALUES(5,2,1,"error",0);
+INSERT INTO feedback VALUES(6,2,2,"question",0);
+INSERT INTO feedback VALUES(7,2,3,"mood",1);
+INSERT INTO feedback VALUES(8,2,4,"response",1);
+INSERT INTO feedback VALUES(9,3,1,"error",1);
+INSERT INTO feedback VALUES(10,3,2,"question",0);
+INSERT INTO feedback VALUES(11,3,3,"mood",1);
+INSERT INTO feedback VALUES(12,3,4,"response",0);
+INSERT INTO feedback VALUES(13,4,1,"error",0);
+INSERT INTO feedback VALUES(14,4,2,"question",1);
+INSERT INTO feedback VALUES(15,4,3,"mood",0);
+INSERT INTO feedback VALUES(16,4,4,"response",1);
 
-INSERT INTO errors VALUES(1,"Audio","We can't hear you");
-INSERT INTO errors VALUES(5,"Video","Screen not visible");
-INSERT INTO errors VALUES(9,"Audio","Microphone not turned on");
-INSERT INTO errors VALUES(13,"Video","Very pixelated and laggy");
+INSERT INTO errors VALUES(1,"audio","We can't hear you");
+INSERT INTO errors VALUES(5,"video","Screen not visible");
+INSERT INTO errors VALUES(9,"audio","Microphone not turned on");
+INSERT INTO errors VALUES(13,"video","Very pixelated and laggy");
 
 INSERT INTO questions VALUES(2,"What is a monad?");
 INSERT INTO questions VALUES(6,"How do I implement multithreading?");
 INSERT INTO questions VALUES(10,"When should I backtrack in a search?");
 INSERT INTO questions VALUES(14,"Why is dynamic programming efficient?");
 
-INSERT INTO moods VALUES(1,3,"Text",3,30);
-INSERT INTO moods VALUES(2,7,"Emoji",2,12);
-INSERT INTO moods VALUES(3,11,"Text",4,25);
-INSERT INTO moods VALUES(4,15,"Emoji",-1,44);
+INSERT INTO moods VALUES(1,3,"text",3,30);
+INSERT INTO moods VALUES(2,7,"emoji",2,12);
+INSERT INTO moods VALUES(3,11,"text",4,25);
+INSERT INTO moods VALUES(4,15,"emoji",-1,44);
 
 INSERT INTO text_moods VALUES(1,"This lecture is going very well, good job!");
 INSERT INTO text_moods VALUES(3,"I can't believe how absolutely amazing this event is. I love it!");
@@ -62,10 +62,10 @@ INSERT INTO text_moods VALUES(3,"I can't believe how absolutely amazing this eve
 INSERT INTO emoji_moods VALUES(2,":smile:");
 INSERT INTO emoji_moods VALUES(4,":angry:");
 
-INSERT INTO responses VALUES(1,4,"Text","Express f = \x -> \y -> x * y using syntactic sugar");
-INSERT INTO responses VALUES(2,8,"Emoji","How comfortable are you with mutex locks?");
-INSERT INTO responses VALUES(3,12,"MultChoice","True or False: A rational agent is successful all of the time");
-INSERT INTO responses VALUES(4,16,"Text","What is the asymptotic running time of the early-finish-time first algorithm?");
+INSERT INTO responses VALUES(1,4,"text","Express f = \x -> \y -> x * y using syntactic sugar");
+INSERT INTO responses VALUES(2,8,"emoji","How comfortable are you with mutex locks?");
+INSERT INTO responses VALUES(3,12,"multchoice","True or False: A rational agent is successful all of the time");
+INSERT INTO responses VALUES(4,16,"text","What is the asymptotic running time of the early-finish-time first algorithm?");
 
 INSERT INTO text_responses VALUES(1,"f x y = x * y");
 INSERT INTO text_responses VALUES(4,"O(nlogn)");
--- dbtest.py
+++ dbtest.py
@@ -2,10 +2,21 @@ from classes import *
 
 connection = DBController()
 
-testError = ErrorFeedback(True,1,1,"Test","Just testing the feedback!")
-connection.insertError(testError)
+# connection.createEvent(3, "Software Engineering")
+# connection.createEvent(4, "Formal Languages")
 
-testQuestion = QuestionFeedback(False,2,3,"What is a rational agent?")
-connection.insertQuestion(testQuestion)
+# connection.addAttendee(5,3)
+
+# testError = ErrorFeedback(True,1,1,"Test","Just testing the feedback!")
+# connection.insertError(testError)
+
+# testQuestion = QuestionFeedback(False,2,3,"What is a rational agent?")
+# connection.insertQuestion(testQuestion)
+
+# testTextMood = TextMood(True,4,2,"text",0.5,"Pretty good")
+# connection.insertMood(testTextMood)
+
+# testEmojiMood = EmojiMood(False,3,4,"emoji",-0.8,":confused:") # int or string?
+# connection.insertMood(testEmojiMood)
 
 connection.close()
