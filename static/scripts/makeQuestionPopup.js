efa04dea364d658b571dbd1d389ec49b73a38d9d
Manojj Ranjan <manojj.ranjan@gmail.com>
2021-03-01 13:53:15 +0000

    Altered multiple choice options class

--- static/scripts/makeQuestionPopup.js
+++ static/scripts/makeQuestionPopup.js
@@ -13,13 +13,13 @@ function questionTypeUpdate() {
     var inputer = document.getElementById("questionInputer");
 
     if (questionType === "normal") {
-        var htmlString = `<input type"text" id="mainQuestion" name="mainQuestion"><br>`;
+        var htmlString = `<input type="text" id="mainQuestion" name="mainQuestion"><br>`;
         htmlString += `<input type="button" onclick="submitNormalQuestion()" value="Submit">`;   
     }
     if (questionType === "multichoice") {
-        var htmlString = `<input type"text" id="mainQuestion" name="mainQuestion"><br>`;
-        htmlString += `<input type"text" id="option1" name="option1"><br>`;
-        htmlString += `<input type"text" id="option2" name="option2"><br>`;
+        var htmlString = `<input type="text" id="mainQuestion" name="mainQuestion"><br>`;
+        htmlString += `<input type="text" id="option1" name="option1"><br>`;
+        htmlString += `<input type="text" id="option2" name="option2"><br>`;
         htmlString += `<input type="button" onclick="addAdditionalChoice()" value="Add More Choices">`;
         htmlString += `<input type="button" onclick="submitMultiChoiceQuestion()" value="Submit">`;
     }
@@ -40,11 +40,12 @@ function submitNormalQuestion() {
     closePopup();
 }
 
-
 function submitMultiChoiceQuestion() {
     var newQuestion = new Question("multichoice");
     newQuestion.setQuestion(document.getElementById("mainQuestion").value);
-    //newQuestion.addChoice(document.getElementById("option1").value);
+    for (var i = 1; i < counter + 1; i++) {
+        newQuestion.addChoice(document.getElementById("option"+i).value);
+        }
     questionTemplate.addQuestion(newQuestion);
     onTemplateUpdate();
     closePopup();
@@ -53,6 +54,6 @@ function submitMultiChoiceQuestion() {
 function addAdditionalChoice() {
     counter += 1;
     var inputer = document.getElementById("questionInputer");
-    inputer.innerHTML += `<input type"text" id="option"+counter name="option"+counter><br>`;
+    inputer.innerHTML += `<input type="text" id="option`+ counter.toString() +`"  name="option`+ counter.toString() +`"><br>`;
 }
 
