89517ed7962c6b92ec7c5b231ed0f0af1c3d4ed9
tuck-ab <alext.5@hotmail.co.uk>
2021-03-07 15:00:16 +0000

    Can now search through meetings

--- static/classes/searchResults.js
+++ static/classes/searchResults.js
@@ -1,25 +1,38 @@
 class SearchResults {
     constructor() {
         this.results = [];
+        this.filtered_results = [];
     }
 
-    setResults(results) {
+    setResults(results, search) {
         this.results = results;
+        this.setFilteredResults(search);
+    }
+    
+    setFilteredResults(keyword) {
+        keyword = keyword.toLowerCase()
+        this.filtered_results = [];
+
+        for (var i = 0; i < this.results.length; i++) {
+            if (this.results[i]["title"].toLowerCase().includes(keyword)) {
+                this.filtered_results.push(this.results[i])
+            }
+        }
     }
 
     getResultString(id) {
-        return `{"meetingid":"` + this.results[id]["meeting_id"] + `"}`;
+        return `{"meetingid":"` + this.filtered_results[id]["meeting_id"] + `"}`;
     }
 
     getHTMLString() {
         var HTMLString = "";
         var loopString = "";
 
-        for (var i = 0; i < this.results.length; i++) {
+        for (var i = 0; i < this.filtered_results.length; i++) {
             loopString = `<div class="searchResult" onclick="selectResult(` + i.toString() + `)">`
-            loopString += `<p>Title: ` + this.results[i]["title"] + `</p>`;
-            loopString += `<p>Start Time: ` + this.results[i]["date_time"] + `</p>`;
-            loopString += `<p>Duration: ` + this.results[i]["run_time"] + `</p>`;
+            loopString += `<p>Title: ` + this.filtered_results[i]["title"] + `</p>`;
+            loopString += `<p>Start Time: ` + this.filtered_results[i]["date_time"] + `</p>`;
+            loopString += `<p>Duration: ` + this.filtered_results[i]["run_time"] + `</p>`;
             loopString += `</div>`
 
             HTMLString += loopString;
